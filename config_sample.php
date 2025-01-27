28e17b4b6cb6904c74ccd4dffbba4d4381e21088
Natihermida <hermidaltamarnati@gmail.com>
2025-01-27 20:24:43 +0100

    cambios

---
+++ config_sample.php
@@ -0,0 +1,7 @@
+<?php
+
+define("HOST", "servidor");
+define("DB_USER", "usuario");
+define("DB_PASS", "contraseña");
+define("DB_DATABASE", "contraseña");
+?>
--- index.php
+++ index.php
@@ -1,9 +1,42 @@
+<?php
+if(isset($_POST["username"])){
+    try{include("conexiondb.php");
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $sql = "SELECT * FROM usuarios WHERE username = :username";
+        $stm=$conexion->prepare($sql);
+        $stm->bindParam("username",$username);
+        $stm->execute();
+        $row=$stm->fetch(PDO::FETCH_ASSOC);
+    
+        if ($row) {
+            if (password_verify($password, $row["password"])) {
+                session_start();
+                $_SESSION["username"] = $username;
+                header("Location: tienda");
+            } else {
+                $error = "Usuario o contraseña incorrectos";
+            }
+        } else {
+            $error = "Usuario o contraseña incorrectos";
+        }
+    } catch (Exception $e) {
+        
+        $error="Error al iniciar sesión, contacte con el administrador".$e->getMessage();
+        echo "<br>";
+        echo DB_USER. "  ".DB_PASS;
+    }
+    }
+    
+
+ ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tienda</title>
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
     <form action="" method="post" >
@@ -13,6 +46,9 @@
     <label for="password">Contraseña</label>
     <input type="password" name="password" id="password" required placeholder="password">
     <input type="submit" value="Iniciar sesión">
+    <?php if(isset($error)){
+        echo "<p>".$error."<p>";
+    } ?>
     </form>
 </body>
 </html>
